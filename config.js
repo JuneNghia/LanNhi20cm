dd2450827f11cc3e0908005773488c0f59071b50
styvenxnguyen <65864310+styvenxnguyen@users.noreply.github.com>
2021-08-08 21:41:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Nhi khùnnnn của anh ♥️',
+    introDesc: `Nếu nhấp vào nút dưới đây là em phải có trách nhiệm dới anh đó nha, vì một khi nhấn rùi là quay lại không được nữa đâuu, đừng có chơi dơ mà tải lại trang á nha =))`,
+    btnIntro: 'Click vào đây nè em iuuu <3',
+    title: 'Sa rang haeeeee 😙',
+    desc: 'Bấm vào đây rùi thì giờ thấy hối hận chưa, cho quyết định thêm một lần nữa nè, suy nghĩ kĩ trước khi nhấp chuột nhaa, chọn Tăng hay Giảm :)))))))))) (Có cái nhạc bên góc trái đó, mở nghe nhaaaaa)',
+    btnYes: 'Có, em yêu anh',
+    btnNo: 'Kó, em kó yêu anh =))',
+    question:'Em có muốn cùng pụng péo này đi dạo Sài Gòn mỗi đêm hông 🌃',
+    btnReply: 'Xác Nhận',
+    reply: 'Dạ CÓ, không có lưa chọn khác đâu kk =)), em phải ở bên anhhhh đóooo',
+    mess: 'Ui thích quá 🥰. Anh cũng muốn cùng cái eo 60 có một tình iuu thật đẹppp 😘😘',
+    messDesc: 'Đợi hết mùa dịch này, anh qua đón em đi ăn khắp Sài Gòn dới anh nháaaaa, Anh yêu Em 💏',
+    btnAccept: 'Nhấp vào đây để nhắn với người yêu em 💕',
+    messLink: 'https://www.messenger.com/t/100007876893507' 
+}
